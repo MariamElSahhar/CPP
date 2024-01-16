22a28d54808832868ecf1eb4326e4249356f0a1a
MariamElSahhar <me1693@nyu.edu>
2024-01-16 18:02:53 +0400

    main

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,13 +6,20 @@
 /*   By: melsahha <melsahha@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 17:56:04 by melsahha          #+#    #+#             */
-/*   Updated: 2024/01/16 17:56:24 by melsahha         ###   ########.fr       */
+/*   Updated: 2024/01/16 18:02:12 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int main() {
-    
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
     return 0;
 }
