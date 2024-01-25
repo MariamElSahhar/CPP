34c7176fc02d9add7289d736b8a0c078c2311fd9
MariamElSahhar <me1693@nyu.edu>
2024-01-25 21:07:53 +0400

    cpp03 ex00 explicit destructure def

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: melsahha <melsahha@student.42abudhabi.a    +#+  +:+       +#+        */
+/*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 11:55:22 by melsahha          #+#    #+#             */
-/*   Updated: 2024/01/24 17:45:51 by melsahha         ###   ########.fr       */
+/*   Updated: 2024/01/25 21:07:28 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@
 
 class ClapTrap {
     private:
-        std::string _name;   
+        std::string _name;
         unsigned int _hit;
         unsigned int _energy;
         unsigned int _attack;
     public:
         ClapTrap (std::string n);
-    
+        ~ClapTrap ();
+
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
