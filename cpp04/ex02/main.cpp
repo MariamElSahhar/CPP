0f0975cdf04c6d0f4f4580c7468feca930621752
MariamElSahhar <me1693@nyu.edu>
2024-01-28 13:30:50 +0400

    cpp04 ex02 tests

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 20:12:12 by melsahha          #+#    #+#             */
-/*   Updated: 2024/01/27 18:58:23 by melsahha         ###   ########.fr       */
+/*   Updated: 2024/01/28 13:30:18 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,22 @@
 #include "WrongCat.hpp"
 
 int main() {
+	// Animal cat;
 	// const Animal* cat = new Animal();
-	// const Animal *cat = new Cat();
-	// const Cat *cat = new Cat();
+	const Animal *cat = new Cat();
+	const Cat *cat2 = new Cat();
+	const Animal *dog = new Dog();
+	const Dog *dog2 = new Dog();
 
+	// cat.makeSound();
 	cat->makeSound();
+	cat2->makeSound();
+	dog->makeSound();
+	dog2->makeSound();
 
 	delete cat;
+	delete cat2;
+	delete dog;
+	delete dog2;
 	return (0);
 }
