cad86a86853dcc29a3d2ba73b69585feae9c9114
MariamElSahhar <me1693@nyu.edu>
2024-01-29 17:54:48 +0400

    cpp01 ex00 tests

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 22:01:37 by melsahha          #+#    #+#             */
-/*   Updated: 2024/01/26 18:37:26 by melsahha         ###   ########.fr       */
+/*   Updated: 2024/01/29 17:53:57 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,25 @@
 #include "Zombie.hpp"
 
 int main() {
-    Zombie zombie("sunflower");
-    Zombie* peashooter = newZombie("peashooter");
-    Zombie* repeater = new Zombie("repeater");
+    {
+		std::cout << std::endl << "----Testing zombie class----" << std::endl;
+		Zombie zombie("sunflower");
+		Zombie* repeater = new Zombie("repeater");
+    	zombie.announce();
+    	repeater->announce();
+	    delete repeater;
+    }
 
-    zombie.announce();
-    peashooter->announce();
-    repeater->announce();
+	{
+		std::cout << std::endl << "----Testing newZombie class----" << std::endl;
+		Zombie* peashooter = newZombie("peashooter");
+	    peashooter->announce();
+	    delete peashooter;
+	}
 
-	randomChump("wallnut");
 
-    delete peashooter;
-    delete repeater;
+	{
+		std::cout << std::endl << "----Testing randomChump class----" << std::endl;
+		randomChump("wallnut");
+	}
 }
