b4f4f38a999b458f450a48767f54b92e25ee507f
MariamElSahhar <me1693@nyu.edu>
2024-01-29 18:01:53 +0400

    cpp01 ex01 test

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 22:33:14 by melsahha          #+#    #+#             */
-/*   Updated: 2024/01/26 19:03:47 by melsahha         ###   ########.fr       */
+/*   Updated: 2024/01/29 18:01:33 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ int main() {
 
 	Zombie* horde = zombieHorde(num, "peashooter");
 
-	horde->announce();
+	for (int i = 0; i < num; i++)
+		horde[i].announce();
+
+	std::cout << std::endl << "----Proving pointer is to first zombie----" << std::endl;
+	horde->setName("repeater");
 
 	for (int i = 0; i < num; i++)
 		horde[i].announce();
