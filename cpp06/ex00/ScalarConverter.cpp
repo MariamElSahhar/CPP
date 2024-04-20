7cb8bce1eae073e402846bf0b652bdf303e2b017
MariamElSahhar <me1693@nyu.edu>
2024-04-20 18:11:23 +0400

    cpp06 ex00 fix

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: melsahha <melsahha@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 09:19:06 by melsahha          #+#    #+#             */
-/*   Updated: 2024/04/17 18:41:38 by melsahha         ###   ########.fr       */
+/*   Updated: 2024/04/20 18:09:12 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,25 @@ ScalarConverter::ScalarConverter()
 	std::cout << "ScalarConverter constructor called." << std::endl;
 }
 
-ScalarConverter::ScalarConverter( ScalarConverter const &s )
+ScalarConverter::ScalarConverter(ScalarConverter const &s)
 {
 	(void)s;
 	std::cout << "ScalarConverter copy constructor called." << std::endl;
 }
 
-ScalarConverter&	ScalarConverter::operator=( ScalarConverter const &s ) {
+ScalarConverter &ScalarConverter::operator=(ScalarConverter const &s)
+{
 	std::cout << "ScalarConverter copy assignement constructor called." << std::endl;
 	(void)s;
 	return *this;
 }
 
-ScalarConverter::~ScalarConverter ()
+ScalarConverter::~ScalarConverter()
 {
 	std::cout << "ScalarConverter destructor called." << std::endl;
 }
 
-int	whatNumber(std::string s)
+int whatNumber(std::string s)
 {
 	if (s.length() == 1 && std::isalpha(s[0]))
 		return (CHAR);
@@ -73,31 +74,119 @@ int	whatNumber(std::string s)
 
 int ScalarConverter::converttoInt(std::string s)
 {
-	int	type = whatNumber(s);
-
-	if (type == INTEGER)
+	if (isInt(s))
 		return (std::stoi(s));
-	else if (type == FLOAT)
-		return (static_cast<int>(std::stof(s)));
-	else if (type == DOUBLE)
+	else if (isDouble(s))
 		return (static_cast<int>(std::stod(s)));
-	else if (type == CHAR)
+	else if (isFloat(s))
+		return (static_cast<int>(std::stof(s)));
+	else if (s.length() == 1 && (isalpha(s[0]) || ispunct(s[0]) || s[0] == ' '))
 		return (static_cast<int>(s[0]));
 	else
 		return (0);
 }
 
+int ScalarConverter::isInt(std::string s)
+{
+	int i = 0;
+	if (s[i] && (s[i] == '-' || s[i] == '+'))
+		i++;
+	while (s[i] && isdigit(s[i]))
+		i++;
+	if (s[i] && !isdigit(s[i]))
+		return (0);
+	try
+	{
+		std::stoi(s);
+		return (1);
+	}
+	catch (...)
+	{
+		return (-1);
+	}
+	return (0);
+}
+
+int ScalarConverter::isDouble(std::string s)
+{
+	int i = 0;
+	if (s[i] && (s[i] == '-' || s[i] == '+'))
+		i++;
+	while (s[i] && isdigit(s[i]))
+		i++;
+	if (!s[i] || (s[i] && s[i] != '.'))
+		return (0);
+	else if (s[i] && s[i] == '.')
+	{
+		i++;
+		if (!s[i] || (s[i] && !isdigit(s[i])))
+			return (0);
+		while (s[i] && isdigit(s[i]))
+			i++;
+		if (!s[i])
+		{
+			try
+			{
+				std::stod(s);
+				return (1);
+			}
+			catch (...)
+			{
+				return (-1);
+			}
+		}
+	}
+	return (0);
+}
+
+int ScalarConverter::isFloat(std::string s)
+{
+	size_t i = 0;
+	if (s[i] && (s[i] == '-' || s[i] == '+'))
+		i++;
+	while (s[i] && isdigit(s[i]))
+		i++;
+	if (!s[i] || (s[i] && s[i] != '.'))
+		return (0);
+	else if (s[i] && s[i] == '.')
+	{
+		i++;
+		if (!s[i] || (s[i] && !isdigit(s[i])))
+			return (0);
+		while (s[i] && isdigit(s[i]))
+			i++;
+		if (s[i] && s[i] == 'f' && i == (s.length() - 1))
+		{
+			try
+			{
+				std::stof(s);
+				return (1);
+			}
+			catch (...)
+			{
+				return (-1);
+			}
+		}
+	}
+	return (0);
+}
+
+bool ScalarConverter::isChar(std::string s)
+{
+	return (s.length() == 1 && (isalpha(s[0]) || ispunct(s[0]) || s[0] == ' '));
+}
+
 void ScalarConverter::printChar(std::string s)
 {
 	std::cout << "char: ";
-	if (s.length() == 1 && (isalpha(s[0]) || ispunct(s[0]) || s[0] == ' '))
+	if (isChar(s))
 		std::cout << s[0];
 	else if (s == "nan" || s == "inf" || s == "+inf" || s == "-inf" || s == "inff" || s == "+inff" || s == "-inff")
 		std::cout << "impossible";
-	else if (whatNumber(s) && (converttoInt(s) > 0 && converttoInt(s) < 33))
+	else if ((isInt(s) == 1) && (std::stoi(s) > 0 && std::stoi(s) < 33))
 		std::cout << "Non displayable";
-	else if (whatNumber(s) && (converttoInt(s) > 32 && converttoInt(s) < 127))
-		std::cout << static_cast<char>(converttoInt(s));
+	else if ((isInt(s) == 1) && (std::stoi(s) > 32 && std::stoi(s) < 127))
+		std::cout << static_cast<char>(std::stoi(s));
 	else
 		std::cout << "impossible";
 	std::cout << std::endl;
@@ -106,10 +195,25 @@ void ScalarConverter::printChar(std::string s)
 void ScalarConverter::printInt(std::string s)
 {
 	std::cout << "int: ";
-	 if (s == "nan" || s == "inf" || s == "+inf" || s == "-inf" || s == "inff" || s == "+inff" || s == "-inff")
+	if (isChar(s))
+		std::cout << static_cast<int>(s[0]);
+	else if (s == "nan" || s == "inf" || s == "+inf" || s == "-inf" || s == "inff" || s == "+inff" || s == "-inff")
 		std::cout << "impossible";
-	else if (whatNumber(s))
-		std::cout << converttoInt(s);
+	else if (isInt(s) == 1)
+		std::cout << std::stoi(s);
+	else if (isInt(s) == -1 || isDouble(s) == -1 || isFloat(s) == -1)
+		std::cout << "overflow";
+	else if (isDouble(s) == 1 || isFloat(s) == 1)
+	{
+		try
+		{
+			std::cout <<std::stoi(s);
+		}
+		catch (...)
+		{
+			std::cout << "overflow";
+		}
+	}
 	else
 		std::cout << "nan";
 	std::cout << std::endl;
@@ -118,26 +222,42 @@ void ScalarConverter::printInt(std::string s)
 void ScalarConverter::printFloat(std::string s)
 {
 	std::cout << "float: ";
-	 if (s == "nan" || s == "inf" || s == "-inf")
-	 	std::cout << s << "f";
- 	else if (s == "+inf" || s == "+inff")
-	 	std::cout << "inff";
+	if (s == "nan" || s == "inf" || s == "-inf")
+		std::cout << s << "f";
+	else if (s == "+inf" || s == "+inff")
+		std::cout << "inff";
 	else if (s == "inff" || s == "-inff")
 		std::cout << s;
-	else if (whatNumber(s) == INTEGER)
-		std::cout << converttoInt(s) << ".0f";
-	else if (whatNumber(s) == DOUBLE) {
-			double d = std::stod(s);
-			if (d == static_cast<int>(d)) {
-				std::cout << std::fixed << std::setprecision(1) << d << "f";
-			} else {
-				std::cout << d << "f";
-			}
+	else if (isInt(s) != 0)
+	{
+		try
+		{
+			std::stof(s);
+			std::cout << s << ".0f";
+		}
+		catch (...)
+		{
+			std::cout << "overflow";
+		}
 	}
-	else if (whatNumber(s) == FLOAT)
+	else if (isDouble(s) != 0)
+	{
+		try
+		{
+			std::stof(s);
+			std::cout << s << "f";
+		}
+		catch (...)
+		{
+			std::cout << "overflow";
+		}
+	}
+	else if (isFloat(s) == 1)
 		std::cout << s;
-	else if (whatNumber(s) == CHAR)
-		std::cout << converttoInt(s) << ".0f";
+	else if (isFloat(s) == -1)
+		std::cout << "overflow";
+	else if (isChar(s))
+		std::cout << static_cast<int>(s[0]) << ".0f";
 	else
 		std::cout << "nan";
 	std::cout << std::endl;
@@ -146,20 +266,42 @@ void ScalarConverter::printFloat(std::string s)
 void ScalarConverter::printDouble(std::string s)
 {
 	std::cout << "double: ";
-	 if (s ==  "nan" || s == "inf" || s == "-inf")
-	 	std::cout << s;
- 	else if (s == "+inf" || s == "inff" || s == "+inff")
+	if (s == "nan" || s == "inf" || s == "-inf")
+		std::cout << s;
+	else if (s == "+inf" || s == "inff" || s == "+inff")
 		std::cout << "inf";
 	else if (s == "-inff")
 		std::cout << "-inf";
-	else if (whatNumber(s) == INTEGER)
-		std::cout << converttoInt(s) << ".0";
-	else if (whatNumber(s) == DOUBLE)
-		std::cout << std::stod(s);
-	else if (whatNumber(s) == FLOAT)
-		std::cout << s.substr(0, s.length() - 1);
-	else if (whatNumber(s) == CHAR)
-		std::cout << converttoInt(s) << ".0";
+	else if (isInt(s) != 0)
+	{
+		try
+		{
+			std::stod(s);
+			std::cout << s << ".0";
+		}
+		catch (...)
+		{
+			std::cout << "overflow";
+		}
+	}
+	else if (isDouble(s) == 1)
+		std::cout << s;
+	else if (isDouble(s) == -1)
+		std::cout << "overflow";
+	else if (isFloat(s) != 0)
+	{
+		try
+		{
+			std::stod(s);
+			std::cout << s.substr(0, s.length() - 1);
+		}
+		catch (...)
+		{
+			std::cout << "overflow";
+		}
+	}
+	else if (isChar(s))
+		std::cout << static_cast<int>(s[0]) << ".0";
 	else
 		std::cout << "nan";
 	std::cout << std::endl;
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: melsahha <melsahha@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 09:18:36 by melsahha          #+#    #+#             */
-/*   Updated: 2024/04/17 17:50:50 by melsahha         ###   ########.fr       */
+/*   Updated: 2024/04/20 17:47:43 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,22 @@ private:
 	~ScalarConverter();
 	ScalarConverter(const ScalarConverter &s);
 	ScalarConverter& operator=(const ScalarConverter &s);
+	// Type Checks
+	static int isInt(std::string s);
+	static int isDouble(std::string s);
+	static int isFloat(std::string s);
+	static bool isChar(std::string s);
+
+	// Converters
+	static int		converttoInt(std::string s);
+	static float	converttoFloat(std::string s);
+	static double	converttoDouble(std::string s);
+
+	// Printers
 	static void		printChar(std::string s);
 	static void		printInt(std::string s);
 	static void		printFloat(std::string s);
 	static void		printDouble(std::string s);
-	static int		converttoInt(std::string s);
-	static float	converttoFloat(std::string s);
-	static double	converttoDouble(std::string s);
 
 public:
 	static void convert(std::string s);
